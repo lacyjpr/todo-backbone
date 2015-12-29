13bcc1cab9873493d9c3ea737472fb45496b5cb0
Jean-Paul Lacy <lacyjpr@gmail.com>
2015-12-29 11:47:49 -0800

    fix: Attempt to fix login issue

--- js/app.js
+++ js/app.js
@@ -6,13 +6,18 @@ var ENTER_KEY = 13;
 $(function() {
 
   var ref = new Firebase("https://blinding-torch-1635.firebaseIO.com");
-  ref.authWithOAuthRedirect("google", function(error, authData) {
-    if (error) {
-      console.log("Login Failed!", error);
-    } else {
-      // kick off app
-      new app.AppView();
-    }
-  });
+  var authData = ref.getAuth();
+  if (authData) {
+    ref.authWithOAuthRedirect("google", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        // kick off app
+        new app.AppView();
+      }
+    });
+  } else {
+     new app.AppView();
+  }
 
 });
