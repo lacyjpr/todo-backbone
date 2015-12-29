beb5701ac3cae38d9bf18f3e8c9a7a41e8e2e690
Jean-Paul Lacy <lacyjpr@gmail.com>
2015-12-29 14:24:12 -0800

    fix: Revert changes

--- js/app.js
+++ js/app.js
@@ -1,23 +1,12 @@
 // js/app.js
 
-var app = app || {};
-var ENTER_KEY = 13;
+  var app = app || {};
+  var ENTER_KEY = 13;
 
-$(function() {
+  $(function() {
 
-  var ref = new Firebase("https://blinding-torch-1635.firebaseIO.com");
-  var authData = ref.getAuth();
-  if (authData) {
-    ref.authWithOAuthRedirect("google", function(error, authData) {
-      if (error) {
-        console.log("Login Failed!", error);
-      } else {
-        // kick off app
-        new app.AppView();
-      }
-    });
-  } else {
-     new app.AppView();
-  }
+    // Kick things off by creating the **App**.
+    new app.AppView();
 
-});
+  });
+
