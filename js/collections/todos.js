62fc8ad1bc83743428cc6882fa1a2811c120aa9d
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-01-24 20:35:40 -0800

    fix: Switch back to original auth code

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -6,27 +6,27 @@
   // ---------------
 
   // Get the uid of the user so we can save data on a per user basis
-    // var ref = new Firebase("https://blinding-torch-1635.firebaseio.com");
-    // var authData = ref.getAuth();
+    var ref = new Firebase("https://blinding-torch-1635.firebaseio.com");
+    var authData = ref.getAuth();
 
-    // if (authData) {
-    //   window.uid = authData.uid;
-    // console.log(uid);
-    // } else {
-    //   console.log(undefined);
-    // }
+    if (authData) {
+      window.uid = authData.uid;
+    console.log(uid);
+    } else {
+      console.log(undefined);
+    }
 
-  // function authDataCallback(authData) {
-  //   if (authData) {
-  //     var uid = authData.uid;
-  //   } else {
-  //     console.log("User is logged out");
-  //   }
-  // }
+  function authDataCallback(authData) {
+    if (authData) {
+      var uid = authData.uid;
+    } else {
+      console.log("User is logged out");
+    }
+  }
 
-  // // Register the callback to be fired every time auth state changes
-  // var ref = new Firebase("https://blinding-torch-1635.firebaseio.com");
-  // ref.onAuth(authDataCallback);
+  // Register the callback to be fired every time auth state changes
+  var ref = new Firebase("https://blinding-torch-1635.firebaseio.com");
+  ref.onAuth(authDataCallback);
 
   // The collection of todos is backed by firebase instead of localstorage
   var TodoList = Backbone.Firebase.Collection.extend({
@@ -35,7 +35,7 @@
     model: app.Todo,
 
     // Save all of the todos to firebase
-    // url: "https://blinding-torch-1635.firebaseio.com/" + window.uid,
+    url: "https://blinding-torch-1635.firebaseio.com/" + window.uid,
 
     // Set url based on uid (passed from AppView)
     // credit sarah_m https://github.com/sarah-maris/FEND-health-tracker
@@ -43,16 +43,16 @@
     //     this.url = params.url;
     // },
 
-    url: function() {
-      var base_url = "https://blinding-torch-1635.firebaseio.com";
-      var ref = new Firebase("https://blinding-torch-1635.firebaseio.com");
-      var authData = ref.getAuth();
-      if (authData) {
-        return new Firebase("https://blinding-torch-1635.firebaseio.com" + authData.uid);
-        } else {
-        return new Firebase(base_url);
-        }
-      },
+    // url: function() {
+    //   var base_url = "https://blinding-torch-1635.firebaseio.com";
+    //   var ref = new Firebase("https://blinding-torch-1635.firebaseio.com");
+    //   var authData = ref.getAuth();
+    //   if (authData) {
+    //     return new Firebase("https://blinding-torch-1635.firebaseio.com" + authData.uid);
+    //     } else {
+    //     return new Firebase(base_url);
+    //     }
+    //   },
 
     // Fetch the new collection
     // get: function() {
